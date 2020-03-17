14db8410c11b00c60a0046fdc399b40b9a867f3d
RobertMisch <robertmisch94@gmail.com>
2020-03-17 16:54:29 -0400

    obtained mvp

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -11,15 +11,15 @@ const LikeSection = props => {
       key="likes-icons-container"
     >
       <div className="like-section-wrapper">
-        <i className="far fa-heart" />
+        <i className="far fa-heart" onClick={props.incrementLikes}/>
       </div>
       <div className="like-section-wrapper">
         <i className="far fa-comment" />
       </div>
     </div>
     <p className="like-number">
-      
-      likes</p>
+      {console.log(props)}
+      {props.likeNumber} likes</p>
 </div>
   )
 };
